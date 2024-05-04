7c4e0cbe67e214e9d89ea017c80f06ad9c74c01c
Piyush Mohapatra <piyush.ofcl@gmail.com>
2024-05-05 03:34:09 +0530

    Update sec_10k_downloader.py

--- sec_10k_downloader.py
+++ sec_10k_downloader.py
@@ -10,5 +10,5 @@ company_tickers = ["MSFT", "V", "AAPL"]
 
 for ticker in company_tickers:
     print(f"Starting downloads for {ticker} from 1995 to {current_year}...")
-    dl.get("10-K", ticker, after="1995-01-01")
+    dl.get("10-K", ticker, after="1995-01-01", download_details=True)
     print(f"Completed downloads for {ticker}.")
